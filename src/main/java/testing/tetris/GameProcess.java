40252ed03e2a0818a9535bbe90221bc1b3f86c56
iesub <70565881+iesub@users.noreply.github.com>
2021-10-03 18:14:13 +0300

    Добавлен метод CheckForDoneLines

--- src/main/java/testing/tetris/GameProcess.java
+++ src/main/java/testing/tetris/GameProcess.java
@@ -18,8 +18,42 @@ public class GameProcess {
         }
     }
 
+    public void getScoreForLines(int lines){
+        switch (lines){
+            case 1:
+                score+=100;
+                break;
+            case 2:
+                score+=300;
+                break;
+            case 3:
+                score+=700;
+                break;
+            case 4:
+                score+=1500;
+                break;
+        }
+    }
 
     public void checkForDoneLines(){
-        
+        int doneLineCount = 0;
+        int blockCount = 0;
+        for (int i = 0; i < 20; i++){
+            for (int j = 0; j < 10; j++){
+                if (gameField[i][j] == 1){
+                    blockCount++;
+                }
+            }
+            if (blockCount == 10){
+                doneLineCount++;
+                blockCount = 0;
+            } else {
+                getScoreForLines(doneLineCount);
+                doneLineCount = 0;
+            }
+        }
+        if (doneLineCount != 0){
+            getScoreForLines(doneLineCount);
+        }
     }
 }
